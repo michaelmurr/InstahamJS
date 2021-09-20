6dfd1e3ff915e81cb0f74ac24d3127cd30d1f7e5
michael murr <michael.murr04@gmail.com>
2021-09-20 14:11:24 +0200

    logic for logging user in

---
+++ routes/loginRouter.js
@@ -0,0 +1,12 @@
+import User from "../models/userSchema";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/", (req, res) =>{
+
+
+    //Move stuff from signupRouter over here
+
+    
+});
