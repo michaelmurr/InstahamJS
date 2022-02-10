46b587f9e98c5f50ec1988acb0a70dd4c32ef51a
Michael Murr <michael.murr04@gmail.com>
2022-02-10 20:30:43 +0100

    refactored to functional component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,38 +6,42 @@ import Nav from "./nav";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import useToken from "./useToken";
 
 import SignupForm from "./signupForm";
 import LoginForm from "./loginForm";
 
 const API = "//instahambackend.herokuapp.com";
 
-function App() {
-let postArray = [];
-
+export default function App() {
   const [loading, setLoading] = useState(true);
-  const [posts, setPosts] = useState(postArray);
+  const [posts, setPosts] = useState();
 
-  useEffect(async () => {
-    const response = await fetch(API + "/api/posts");
-    const data = await response.json();
-    setPosts(data);
-    setLoading(false);
-  });
+  const {token, setToken} = useToken();
+  /*
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch(API + "/api/posts");
+      const data = await response.json();
+      setPosts(data);
+      setLoading(false);
+    }
+    fetchData();
+  });*/
+  
+  if(!token) return <Router><LoginForm setToken={setToken}/></Router>
 
-    return (
-      <div className="App">
-        <Router>
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Posts posts={posts} />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<SignupForm />} />
-            <Route path="/api/upload" element={<Upload />} />
-          </Routes>
-        </Router>
-      </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<h1>Renders on "/"</h1>} />
+          <Route path="/login" exact element={<LoginForm />} />
+          <Route path="/register" exact element={<SignupForm />} />
+          <Route path="/api/upload" exact element={<Upload />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
-
-export default App;
