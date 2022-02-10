a1dd2879d141d0245ec11f71e88dba7543474ced
Michael Murr <michael.murr04@gmail.com>
2022-02-10 22:30:45 +0100

    fixed header name

--- backend/routes/verifyToken.js
+++ backend/routes/verifyToken.js
@@ -1,14 +1,15 @@
 import jwt from "jsonwebtoken";
 
 function verify(req, res, next) {
-  const token = req.header("auth-token");
+  const token = req.header("auth");
   if (!token) return res.status(401).send("Access Denied");
-
+  
   try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = payload._id;
     next();
-  } catch (err) {
+  } catch (e) {
+    console.log(e);
     res.status(400).send("Invalid Token");
   }
 }
