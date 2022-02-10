ed0c3596604ae8ffcf9308bde65d7afe4f98fb7b
Michael Murr <michael.murr04@gmail.com>
2022-02-10 17:38:58 +0100

    refactored to a functional component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,36 +1,36 @@
 import "../css/app.css";
 import "../css/posts.css";
-import React from "react";
-import Upload from "./uploadForm";
-import Nav from "../components/nav";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Posts from "./posts";
+import Upload from "./uploadForm";
+import Nav from "./nav";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import SignupForm from "../components/signupForm";
-import LoginForm from "../components/loginForm";
+import SignupForm from "./signupForm";
+import LoginForm from "./loginForm";
 
 const API = "//instahambackend.herokuapp.com";
 
-class App extends React.Component {
-  state = {
-    loading: true,
-    posts: [],
-  };
+function App() {
+let postArray = [];
 
-  async componentDidMount() {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState(postArray);
+
+  useEffect(async () => {
     const response = await fetch(API + "/api/posts");
     const data = await response.json();
-    this.setState({ posts: data, loading: false });
-  }
+    setPosts(data);
+    setLoading(false);
+  });
 
-  render() {
     return (
       <div className="App">
         <Router>
           <Nav />
           <Routes>
-            <Route path="/" element={<Posts posts={this.state.posts} />} />
+            <Route path="/" element={<Posts posts={posts} />} />
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<SignupForm />} />
             <Route path="/api/upload" element={<Upload />} />
@@ -38,7 +38,6 @@ class App extends React.Component {
         </Router>
       </div>
     );
-  }
 }
 
 export default App;
