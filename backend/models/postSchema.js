4b8e600085298406a3abec29a1470bf7ea6638ba
Michael Murr <michael.murr04@gmail.com>
2022-02-12 16:15:44 +0100

    now saving username along with post

--- backend/models/postSchema.js
+++ backend/models/postSchema.js
@@ -1,22 +1,26 @@
 import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-    ownerID:{
-        type: String,
-        required: true,
-    },
-    content:{
-        type: String,
-        required: true
-    },
-    likes:{
-        type: Number,
-        default: 0,
-    },
-    uploadDate:{
-        type: Date,
-        default: Date.now,
-    }
+  ownerID: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export default mongoose.model("Post", postSchema)
+export default mongoose.model("Post", postSchema);
