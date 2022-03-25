ee9cba49ca95474f297423c392419bb8324e7740
Michael Murr <michael.murr04@gmail.com>
2022-03-25 18:17:33 +0100

    refactored code

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -1,33 +1,40 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/nav.css";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Navbar } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import home_icon from "../home_icon.png";
 import upload_icon from "../plus_icon.png";
 import search_icon from "../search_icon.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 import profile_icon from "../profile_icon.png";
 
-function Nav() {
+export default function Nav() {
   return (
-<Navbar fixed="bottom" className="navbar">
-        <ul className="navList">
-            <li>
-                <Link to="/"><img src={home_icon} alt="home icon"/></Link>
-            </li>
-            <li>
-                <Link to="/search"><img src={search_icon} alt="search icon"/></Link>
-            </li>
-          <li>
-            <Link to="/api/upload"><img src={upload_icon} alt="upload icon"/></Link>
-          </li>
-          <li>
-            <Link to="/profile"><img src={profile_icon} alt="upload icon"/></Link>
-
-          </li>
-        </ul>
-</Navbar>
-      /*}
+    <Navbar fixed="bottom" className="j">
+      <ul className="navList">
+        <li>
+          <Link to="/">
+            <img src={home_icon} alt="home icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/search">
+            <img src={search_icon} alt="search icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/api/upload">
+            <img src={upload_icon} alt="upload icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile">
+            <img src={profile_icon} alt="upload icon" />
+          </Link>
+        </li>
+      </ul>
+    </Navbar>
+    /*}
       <Link to="/login">
         <Button>Sign In</Button>
       </Link>
@@ -38,5 +45,3 @@ function Nav() {
   {*/
   );
 }
-
-export default Nav;
