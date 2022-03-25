2647fdec96f8e79b49be80188aaf994f07917218
Michael Murr <michael.murr04@gmail.com>
2022-03-25 18:17:08 +0100

    added missing quotes to header

--- frontend/src/components/signupForm.js
+++ frontend/src/components/signupForm.js
@@ -12,7 +12,7 @@ async function signupUser(credentials) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json",
+      "Accept": "application/json",
     },
     body: JSON.stringify(credentials),
   })
