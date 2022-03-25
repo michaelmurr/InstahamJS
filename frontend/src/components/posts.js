9d38db66b3067b7575b53cc021773550cc55aa01
Michael Murr <michael.murr04@gmail.com>
2022-03-25 18:16:45 +0100

    refactored code

--- frontend/src/components/posts.js
+++ frontend/src/components/posts.js
@@ -1,19 +1,24 @@
 import "../css/posts.css";
-import React from 'react';
+import React from "react";
+import DayJS from "react-dayjs";
+import heart_icon from "../heart_icon.png";
 
-function Posts(props) {
-
-    return (
-        <div className="fetchedPosts">
-            {props.posts.map(post => (
-                <div key={post.ownerID} id={post.ownerID} className="postContainer">
-                    <div>{post.content}</div>
-                    <div>{post.uploadDate}</div>
-                    <div>Likes: {post.likes}</div>
-                </div>
-            ))}
+export default function Posts(props) {
+  return (
+    <div className="fetchedPosts">
+      {props.posts.map((post) => (
+        <div key={post.ownerID} id={post.ownerID} className="postContainer">
+          <h1 className="username">@{post.username}</h1>
+          <div className="postDate">
+            <DayJS format="DD. MMMM YYYY, H:mm">{post.uploadDate}</DayJS>
+          </div>
+          <p>{post.content}</p>
+          <div>
+            <img src={heart_icon} alt="" />
+            {post.likes}
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
-
-export default Posts;
