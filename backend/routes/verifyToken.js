6f47f956b3a097902206b978a62bf029aaa3df20
Michael Murr <michael.murr04@gmail.com>
2022-03-26 13:51:31 +0100

    refactored code

--- backend/routes/verifyToken.js
+++ backend/routes/verifyToken.js
@@ -2,15 +2,17 @@ import jwt from "jsonwebtoken";
 
 function verify(req, res, next) {
   const token = req.header("auth");
-  if (!token) return res.status(401).send("Access Denied");
-  
+
+  if (!token) return res.status(401).send({ message: "Access Denied" });
+
   try {
     const payload = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = payload._id;
+
     next();
   } catch (e) {
     console.log(e);
-    res.status(400).send("Invalid Token");
+    res.status(400).send(e);
   }
 }
 
