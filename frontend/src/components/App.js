a0f53083c1986c7c528a8d17890b525d9becff85
Michael Murr <michael.murr04@gmail.com>
2022-03-29 20:37:14 +0200

    moved fetch into other component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,44 +1,28 @@
 import Nav from "./nav";
 import "../css/app.css";
 import "../css/posts.css";
-import Posts from "./posts";
+import Feed from "./feed";
 import Profile from "./profile";
 import Upload from "./uploadForm";
 import useToken from "./useToken";
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 
 import LoginForm from "./loginForm";
 import SignupForm from "./signupForm";
-
-const API = "https://instahamjs-backend.onrender.com";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
   const { token, setToken } = useToken();
 
-
-  useEffect(() => {
-    async function fetchData() {
-      if (loading === true) {
-        const response = await fetch(API + "/api/posts", { mode: "cors" });
-        const data = await response.json();
-        setLoading(false);
-        setPosts(data.posts);
-        // setDate();
-      }
-    }
-    fetchData();
-  }, []);
-
   return (
     <div className="App">
       <Router>
         <Nav />
         <Routes>
-          <Route path="/" exact element={<Posts posts={posts} />} />
+          <Route path="/" exact element={<Feed/>} />
           <Route
             path="/login"
             exact
