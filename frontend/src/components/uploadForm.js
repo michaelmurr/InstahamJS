98a73792363339a1da4818b28d6d58036aa21bc1
Michael Murr <michael.murr04@gmail.com>
2022-03-29 20:38:55 +0200

    navigates to feed after posting

--- frontend/src/components/uploadForm.js
+++ frontend/src/components/uploadForm.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Form, FloatingLabel } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useState } from "react";
 import useToken from "./useToken";
 import LoginForm from "./loginForm";
 
 const API = "https://instahamjs-backend.onrender.com";
+//const API = "http://localhost:4000";
 
 async function uploadPost(form_data, auth_token) {
   return fetch(API + "/api/upload", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "auth": auth_token
+      auth: auth_token,
     },
     body: JSON.stringify({ contentString: form_data }),
   });
@@ -21,6 +23,7 @@ async function uploadPost(form_data, auth_token) {
 export default function Upload() {
   const [text, setText] = useState("");
   const { token, setToken } = useToken();
+  const navigate = useNavigate();
 
   if (!token) return <LoginForm setToken={setToken} />;
 
@@ -32,14 +35,19 @@ export default function Upload() {
   const handleSubmit = () => async (event) => {
     event.preventDefault();
     const response = await uploadPost(text, token);
-    console.log("Submit response:\n", response);
+    const json = await response.json();
+
+    if (response.status === 200) {
+      navigate("/");
+    } else {
+      console.log(json);
+    }
   };
 
   return (
     <div className="uploadForm">
       <Form onSubmit={handleSubmit()}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label></Form.Label>
           <Form.Control
             as="textarea"
             rows={3}
