de2ea9460ebaf3c8cae275d6cf354c5a69f4360e
Michael Murr <michael.murr04@gmail.com>
2022-03-30 14:59:02 +0200

    accidentally removed required hook

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,7 @@ import "../css/app.css";
 import "../css/posts.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import useToken from "./useToken";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./nav";
@@ -13,12 +14,14 @@ import LoginForm from "./loginForm";
 import SignupForm from "./signupForm";
 
 export default function App() {
+  const { token, setToken } = useToken();
+
   return (
     <div className="App">
       <Router>
         <Nav />
         <Routes>
-          <Route path="/" exact element={<Feed/>} />
+          <Route path="/" exact element={<Feed />} />
           <Route
             path="/login"
             exact
