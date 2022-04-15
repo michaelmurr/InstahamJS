34a1dc2cef795b3e5c3851a39e71dd8802784696
Michael Murr <michael.murr04@gmail.com>
2022-04-15 16:15:31 +0200

    redirects to login if token is missing

--- frontend/src/components/posts.js
+++ frontend/src/components/posts.js
@@ -1,11 +1,15 @@
 import "../css/posts.css";
 import React, { useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom";
 import DayJS from "react-dayjs";
 import heart_icon from "../icons/heart_icon.png";
+import useToken from "./useToken";
 
 export default function Posts(props) {
   const [posts, setPosts] = useState([]);
   const [shouldUpdatePosts, setShouldUpdatePosts] = useState(true);
+  const { token, setToken } = useToken();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (shouldUpdatePosts)
@@ -13,10 +17,10 @@ export default function Posts(props) {
         setPosts(props.posts);
         setShouldUpdatePosts(false);
       }
-    });
-    
-    return (
-      <div className="fetchedPosts">
+  });
+
+  return (
+    <div className="fetchedPosts">
       {posts.map((post) => (
         <div className="postContainer" key={post._id}>
           <h1 className="username">@{post.username}</h1>
@@ -24,7 +28,15 @@ export default function Posts(props) {
             <DayJS format="DD. MMMM YYYY, H:mm">{post.uploadDate}</DayJS>
           </div>
           <p>{post.content}</p>
-          <div onClick={() => setPosts(props.handleLike(post, posts))}>
+          <div
+            onClick={() => {
+              if(token){
+                setPosts(props.handleLike(post, posts));
+              }else{
+                navigate("/login");
+              }
+            }}
+          >
             <img src={heart_icon} alt="" />
             {post.likes}
           </div>
