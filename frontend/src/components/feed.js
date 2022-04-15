50f3076edda01827de91c057980d77ff01fac9da
Michael Murr <michael.murr04@gmail.com>
2022-04-15 16:16:10 +0200

    logic for liking

--- frontend/src/components/feed.js
+++ frontend/src/components/feed.js
@@ -8,7 +8,7 @@ export default function Feed() {
   const [isLoading, setIsLoading] = useState(true);
   const { token } = useToken();
 
-  const API = "https://instahamjs-backend.onrender.com"
+   const API = "https://instahamjs-backend.onrender.com"
   //const API = "http://localhost:4000";
 
   useEffect(() => {
@@ -41,7 +41,6 @@ export default function Feed() {
           items[i] = item;
         }
       }
-
       setPosts(items);
       setLikedPosts(data.liked_posts);
       setIsLoading(false);
@@ -49,40 +48,25 @@ export default function Feed() {
     fetchData();
   }, []);
 
-  function handlePosts() {
-    let test = ["624830bcb3631818f4f4e684", "62483051b3631818f4f4e676"];
-    let items = [...posts];
-
-    for (let i = 0; i <= items.length; i++) {
-      let item = { ...items[i] };
-      for (let j = 0; j <= test; j++) {
-        item._id === test[j]
-        ? (item[i].isLiked = true)
-          : (item[i].isLiked = false);
-          items[i] = item;
-        }
-    }
-    setPosts(items);
-  }
-
-  function handleLike(likedPost, param_posts) {
+  async function handleLike(likedPost, param_posts) {
     let items = [...param_posts];
-
-
     for (let i = 0; i < items.length; i++) {
       if (param_posts[i]._id === likedPost._id) {
         let item = { ...items[i] };
+        console.log(item);
         
         if(item.isLiked){
           item.likes--;
           item.isLiked = false;
+
         }else if (item.isLiked === false){
           item.likes++;
           item.isLiked = true;
         }
 
+
         items[i] = item;
-        console.log(items);
+        console.log("Yup, u got a token");
         return items;
       }
     }
